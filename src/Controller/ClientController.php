6a9580b727da555104a85289a9cb5a770c254e41
aheitzm2 <antoine.heitzmann2@gmail.com>
2018-11-05 13:19:57 +0100

    addProduit

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Entity\TypeProduit;
+use App\Entity\Voitures;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 
 class ClientController extends AbstractController
 {
@@ -15,5 +23,14 @@ class ClientController extends AbstractController
         return $this->render('client/index.html.twig', [
             'controller_name' => 'ClientController',
         ]);
+    }
+
+    /**
+     * @Route("/client/add", name="client.add")
+     * @return Response
+     */
+    public function addProduit(){
+        $types=$this->getDoctrine()->getRepository(TypeProduit::class)->findAll();
+        return $this->render("/frontOff/frontOFFICE_add.html.twig", ["types"=>$types]);
     }
 }
